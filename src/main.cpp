09bb4af25015d98e660ad6067299159302c27036
alex-sidor <astro_studios@outlook.com>
2025-10-04 16:24:41 +0100

    removed sdl and added custom lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-#define SDL_MAIN_USE_CALLBACKS 1
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 #include <cmath>
 #include <iostream>
 #include <algorithm>
 #include <cstdint>
 #include <chrono>
 #include <random>
+
+#include "Screen.h"
 
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 1000
@@ -17,19 +18,15 @@ const int sectorSize = 50;
 int halfWidth = WINDOW_WIDTH/2;
 int halfHeight = WINDOW_HEIGHT/2;
 
-static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
-static SDL_Texture* texture = NULL;
-
 constexpr int amountOfPixels = WINDOW_WIDTH*WINDOW_HEIGHT;
 
-Uint32 pixelBuffer[amountOfPixels];
+uint32_t pixelBuffer[amountOfPixels];
 
 float randomVectors[WINDOW_HEIGHT/sectorSize + 1][WINDOW_WIDTH/sectorSize + 1];
 
 float a = 0;
 
-static void placePoint(int x,int y,Uint32 colour) {
+static void placePoint(int x,int y,uint32_t colour) {
     pixelBuffer[y * WINDOW_WIDTH + x] = colour;
 }
 
@@ -45,13 +42,13 @@ static float fade(float x) {
     return x* x * x * (x * (x * 6 - 15) + 10);
 }
 
-static Uint32 floatToRGBA32(float value) {
+static uint32_t floatToRGBA32(float value) {
     value +=1;
     value /= 2;
 
     uint8_t gray = static_cast<uint8_t>(value * 255.0f);
 
-    Uint32 rgba = (gray << 24) | (gray << 16) | (gray << 8) | 255;
+    uint32_t rgba = (gray << 24) | (gray << 16) | (gray << 8) | 255;
 
     return rgba;
 }
@@ -91,26 +88,13 @@ static void perlin(){
     }
 }
 
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
-{
-    SDL_SetAppMetadata("Example Renderer Streaming Textures", "1.0", "com.example.renderer-streaming-textures");
-
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
+void resetBuffer(){
+    for(int i = 0; i < amountOfPixels; i++){
+        pixelBuffer[i] = 0x000000FF;
     }
+}
 
-    if (!SDL_CreateWindowAndRenderer("Perlin Noise", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WINDOW_WIDTH, WINDOW_HEIGHT);
-    if (!texture) {
-        SDL_Log("Couldn't create streaming texture: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
+int main(){
     std::random_device rd;
     std::default_random_engine gen(rd());
     std::uniform_real_distribution<float> dist(0.0, 6.28318531);
@@ -123,42 +107,22 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
         }
     }
 
-    return SDL_APP_CONTINUE;
-}
+    Screen mainScreen(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-SDL_AppResult SDL_AppIterate(void* appstate)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    SDL_memset4(pixelBuffer, 0x000000FF, amountOfPixels);
-    a += 0.1;
-    perlin();
+    while(!glfwWindowShouldClose(mainScreen.window)){
+        
+        resetBuffer();
+        
+        auto start = std::chrono::high_resolution_clock::now();
+        
+        a += 0.1;
+        perlin();
 
-    SDL_UpdateTexture(texture, NULL, pixelBuffer,WINDOW_WIDTH*sizeof(Uint32));
-    
-    SDL_RenderTexture(renderer, texture, NULL, NULL);
+        mainScreen.displayBuffer(pixelBuffer);
 
-    SDL_RenderPresent(renderer);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << "Elapsed Time: " << duration.count() << " microseconds" << std::endl;
-
-    return SDL_APP_CONTINUE;
-}
-
-void SDL_AppQuit(void* appstate, SDL_AppResult result)
-{
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
-{
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        std::cout << duration.count() << ":microseconds "  <<  1000000.0f/duration.count()  << ":FPS" << std::endl;
     }
-
-    return SDL_APP_CONTINUE;
+     
 }
